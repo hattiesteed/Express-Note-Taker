f56ff73049d3c74ad7bd641e2278df35277eb828
unknown <harriett.steed@gmail.com>
2023-05-18 22:52:39 -0600

    fixed bugs, final commit

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,21 +2,31 @@ const router = require(`express`).Router();
 const fs = require(`fs`);
 
 // get request for api notes page
-router.get(`api/notes`, async (req, res) => {
-    const dbJSON = await JSON.parse(fs.readFileSync(`./Develop/db/db.json`, `utf8`));
-        res.JSON(dbJSON);
+router.get(`/api/notes`, async (req, res) => {
+    const dbJSON = await JSON.parse(fs.readFileSync(`./db/db.json`, `utf8`));
+        res.json(dbJSON);
 });
 
 // post request for api notes page
-router.post(`api/notes`, (req, res) => {
-    const dbJSON = JSON.parse(fs.readFileSync(`./Develop/db/db.json`, `utf8`));
+router.post(`/api/notes`, (req, res) => {
+    const dbJSON = JSON.parse(fs.readFileSync(`./db/db.json`, `utf8`));
     const newNote = {
         title: req.body.title,
         text: req.body.text,
+        id: dbJSON.length +1,
     };
+    console.log(newNote);
     dbJSON.push(newNote);
     fs.writeFileSync(`db/db.json`, JSON.stringify(dbJSON));
-    res.JSON(dbJSON);
+    res.json(dbJSON);
 });
+
+router.delete("/api/notes/:id", (req, res) => {
+    const dbJSON = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
+    const noteId = req.params.id;
+    const newDbJson = dbJSON.filter((note) => note.id != noteId);
+    fs.writeFileSync("db/db.json", JSON.stringify(newDbJson));
+    res.json(newDbJson);
+})
 
 module.exports = router;
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,12 +4,12 @@ const path = require(`path`);
 
 // route for index.html
 router.get(`/`, (req, res) => {
-    res.sendFile(path.join(__dirname, `../Develop/public/index.html`));
+    res.sendFile(path.join(__dirname, `../public/index.html`));
 });
 
 // route for notes.html
 router.get(`/notes`, (req, res) => {
-    res.sendFile(path.join(__dirname, `../Develop/public/notes.html`));
+    res.sendFile(path.join(__dirname, `../public/notes.html`));
 });
 
 module.exports = router;
